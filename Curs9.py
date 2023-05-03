532c12f3b3bd33adc27f8357bbd9ee79033f28a0
Stanica Ionut Ciprian <stanicaciprian@yahoo.com>
2023-05-03 18:17:29 +0300

    functii

--- Curs9.py
+++ Curs9.py
@@ -1,2 +1,13 @@
-#Cursul9
-print("Acesta este cursul 9")
+#Cursul 9. Despre functii
+
+# def f(x):
+#     variabila = x + 2
+#     print(variabila)
+# for i in range(10):
+#     print(i)
+#     f(i)
+#     print("_________")
+     
+def aduna(x,y):
+    print(x + y)
+aduna(2,32)
