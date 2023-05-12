e74f2b3d65dad874c6a28129a2b3df840acc7d33
Stanica Ionut Ciprian <stanicaciprian@yahoo.com>
2023-05-12 14:28:19 +0300

    Curs20 incomplet

--- Curs20.py
+++ Curs20.py
@@ -29,22 +29,41 @@ class Patrat(Dreptunghi):
 # -Avionul militar poate lansa rachete in timp ce zboara(suprascriere)
 # -Avionul militar are cantitate munitie(in tone)
 
-class Avion():
-    def __init__(self):
-        self.zboara = True
-        self.viteza_minima = 100
-        self.consum_combustibil = True
+# class Avion():
+#     def __init__(self,viteza_minima,consum_combustibil):
+#         self.zboara = True
+#         self.viteza_minima = 100
+#         self.consum_combustibil = True
 
-class Avion_Civil(Avion):
-    def __init__(self):
-        self.numar_pasageri = 250
-        self.descarca_bagajele = True
+# class Avion_Civil(Avion):
+#     def __init__(self):
+#         self.numar_pasageri = 250
+#         self.descarca_bagajele = True
 
-class Avion_Militar(Avion):
-    def __init__(self):
-        super().__init__(self)
-        self.lanseaza_rachete = True
-        self.munitie = 300
+# class Avion_Militar(Avion):
+#     def __init__(self):
+#         super().__init__(self)
+#         self.lanseaza_rachete = True
+#         self.munitie = 300
 
-F15 = Avion_Militar()
-print(F15.viteza_minima())
+# F15 = Avion_Militar()
+# print(F15.viteza_minima())
+PATH = "Curs20/"
+import csv
+class Fisier():
+    def __init__(self,nume,dimensiune,tip,cale):
+        self.nume = nume
+        self.dimensiune = dimensiune
+        self.tip = tip
+        self.cale = cale
+class FisierCsv(Fisier):
+    def __init__(self,nume,dimensiune,tip,cale):
+        super().__init__(nume,dimensiune,tip,cale)
+        self.coloane = 0
+        self.randuri = 0
+    def citire_fisier(self):
+        with open(PATH+self.cale,"r") as file:
+            reader = csv.reader(file)
+            continut = csv.load(reader)
+            for line in continut:
+                print(line)
